98bd7748d46e086cad89828a3e9feedd367a90ae
Saksham Patil <sakshampatil815@gmail.com>
2023-02-03 15:42:38 +0530

    created first aggregate stage

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -6,6 +6,8 @@ import { Order, orderDocument } from './schema/order.schema';
 import { Model, mongo, ObjectId } from 'mongoose';
 import { generateRandomString } from 'src/helpers/helpers';
 import * as mongoService from '../helpers/mongoService/mongoService';
+import { Product, productDocument } from 'src/product/schema/product.schema';
+const util = require('util');
 
 @Injectable()
 export class OrderService {
@@ -26,10 +28,51 @@ export class OrderService {
   }
 
   async findOne(id: ObjectId, req) {
+    let products = await mongoService.findOne(this.orderModel, {
+      user: req.user.id,
+      _id: id,
+    });
+    let materials = await this.getMaterialListFromOrder(products.product);
+    products['materials'] = materials;
     return await mongoService.findOne(this.orderModel, {
       user: req.user.id,
       _id: id,
     });
+  }
+
+  async getMaterialListFromOrder(productArray: CreateOrderDto['product']) {
+    let query = [
+      {
+        //Extracting Products
+        $lookup: {
+          from: 'products',
+          localField: 'product.product',
+          foreignField: '_id',
+          as: 'productDetails.products',
+        },
+      },
+      {
+        $unwind: {
+          path: '$product.product',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      // {
+      //   //Extracting Materials
+      //   $lookup: {
+      //     from: 'materials',
+      //     localField: 'productDetails.products.material.material',
+      //     foreignField: '_id',
+      //     as: 'materialDetails',
+      //   },
+      // },
+    ];
+    let ans = await mongoService.aggregate(this.orderModel, query);
+    // console.log('PRODUCT_LIST = ', ans[0]);
+    console.log(
+      'PRODUCT_LIST = ',
+      util.inspect(ans, false, null, true /* enable colors */),
+    );
   }
 
   async update(id: ObjectId, updateOrderDto: UpdateOrderDto, req) {
